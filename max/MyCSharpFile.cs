3233a86c537e8292944d657035d07e2236a5e606
JPPdp <payloyo@gmail.com>
2025-04-13 12:55:19 +0800

    2025-04-13T12:55:19+08:00

--- max/MyCSharpFile.cs
+++ max/MyCSharpFile.cs
@@ -2,7 +2,7 @@ using System;
 // Modified by script: lU0zHgfcie
 // Modified by script: 15aMBSVzF1
 // Modified by script: XaICpk80PY
-    static void Main()
+// Modified by script: GRReiKRco4
     {
 // Modified by script: Enld9tTlcP
 // Modified by script: q57kCrJeEK
